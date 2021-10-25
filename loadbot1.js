7d76d5067eb640399eac76e0d3da3273140c13b2
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2021-10-25 15:18:19 +0530

    Create loadbot1.js

---
+++ loadbot1.js
@@ -0,0 +1,55 @@
+
+const Web3 = require("web3")
+const ContractKit = require('@celo/contractkit')
+const web3 = new Web3('http://localhost:8540')
+const kit = ContractKit.newKitFromWeb3(web3)
+const ethers = require("ethers");
+
+async function height() {
+   for(var i=1;i<=1000000;i++)
+   {
+       await allTrx(i)
+   }
+}
+
+async function allTrx(h)
+{
+    console.log("all trx")
+   
+    let goldtoken = await kit.contracts.getGoldToken()
+
+   for(let i=Math.pow(2,h-1); i<=Math.pow(2,h) -1 ;i++)    // parent node loop
+   {
+    let Dpath="m/44'/52752'/0'/"+i
+    const wallet = ethers.Wallet.fromMnemonic("alien shell toy depth share work clarify tattoo grass tank master board",Dpath)
+    kit.connection.addAccount(wallet.privateKey)
+    let senderAddress =wallet.address
+    let celoBal = await goldtoken.balanceOf(wallet.address)
+    let amount= celoBal.idiv(5)
+
+       for(let j=0;j<=1;j++)                              // child node loop
+       {
+        let child=2*i+j
+        let DpathChild="m/44'/52752'/0'/"+child
+        const walletChild = ethers.Wallet.fromMnemonic("alien shell toy depth share work clarify tattoo grass tank master board",DpathChild)
+        let rec=walletChild.address
+        console.log("inside j")
+        
+        if(i== Math.pow(2,h) -1)
+        {
+            let celotrx =  await goldtoken.transfer(rec, amount).send({from: senderAddress, nonce:j })
+            let celoReceipt2 = await celotrx.waitReceipt()         
+           console.log('CELO Transaction receipt: %o', celoReceipt2)
+
+        }
+        else{
+            let celotrx =   goldtoken.transfer(rec, amount).send({from: senderAddress, nonce:j })
+        }
+         
+       }
+   }
+
+
+}
+
+height();
