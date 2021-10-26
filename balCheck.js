5cb85162e9a899452b994c7938b7aa5ea273a03a
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2021-10-26 16:45:28 +0530

    Update and rename balchecj.js to balCheck.js

--- balCheck.js
+++ balCheck.js
@@ -1,13 +1,11 @@
 const Web3 = require("web3")
 const ContractKit = require('@celo/contractkit')
-const web3 = new Web3('http://localhost:8547')
+const web3 = new Web3('http://localhost:8540')
 const kit = ContractKit.newKitFromWeb3(web3)
 const ethers = require("ethers");
 
 async function send(i) {
     let goldtoken = await kit.contracts.getGoldToken()
-    let stabletoken = await kit.contracts.getStableToken()
-
 
 Dpath="m/44'/52752'/0'/"+i  //derivation path
 //mnemonic ="alien shell toy depth share work clarify tattoo grass tank master board"
