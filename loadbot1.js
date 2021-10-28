84c9c349a5940bf7f4cbb8b4faa8e02fb68393a3
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2021-10-28 19:16:55 +0530

    Update loadbot1.js

--- loadbot1.js
+++ loadbot1.js
@@ -27,35 +27,35 @@ async function allTrx(h)
     let notrx=0;
    
 try {
-   for(let i=Math.pow(2,h-1); i<=Math.pow(2,h) -1 ;i++)    // parent node loop
+   for(var i=Math.pow(2,h-1); i<=Math.pow(2,h) -1 ;i++)    // parent node loop
    {
       
-    let Dpath="m/44'/52752'/0'/"+i
+    var Dpath="m/44'/52752'/0'/"+i
     const wallet = ethers.Wallet.fromMnemonic("alien shell toy depth share work clarify tattoo grass tank master board",Dpath)
     kit.connection.addAccount(wallet.privateKey)
-    let senderAddress =wallet.address
-    let celoBal = await goldtoken.balanceOf(wallet.address)
-    let amount= celoBal.idiv(3)
+    var senderAddress =wallet.address
+    var celoBal = await goldtoken.balanceOf(wallet.address)
+    var amount= celoBal.idiv(3)
 
-       for(let j=0;j<=1;j++)                              // child node loop
+       for(var j=0;j<=1;j++)                              // child node loop
        {
           notrx++
-        let child=2*i+j
-        let DpathChild="m/44'/52752'/0'/"+child
-        const walletChild = ethers.Wallet.fromMnemonic("alien shell toy depth share work clarify tattoo grass tank master board",DpathChild)
-        let rec=walletChild.address
+        var child=2*i+j
+        var DpathChild="m/44'/52752'/0'/"+child
+        var walletChild = ethers.Wallet.fromMnemonic("alien shell toy depth share work clarify tattoo grass tank master board",DpathChild)
+        var rec=walletChild.address
         
         if(i >= Math.pow(2,h) -2 || notrx%10000==0 || notrx%10000==1)
         {
            try {
               try{
-            let celotrx =  await goldtoken.transfer(rec, amount).send({from: senderAddress, nonce:j })
+            var celotrx =  await goldtoken.transfer(rec, amount).send({from: senderAddress, nonce:j })
             }
               catch(err) {
                  console.log("transfer fail catch", err)
               }
               try {
-            let celoReceipt2 = await celotrx.waitReceipt()         
+            var celoReceipt2 = await celotrx.waitReceipt()         
             console.log('CELO Transaction receipt: %o', celoReceipt2)
               }
               catch(err1) {
@@ -74,7 +74,7 @@ try {
         }
         else{
            try {
-            let celotrx =   goldtoken.transfer(rec, amount).send({from: senderAddress, nonce:j })
+            var celotrx =   goldtoken.transfer(rec, amount).send({from: senderAddress, nonce:j })
                     count++;
              console.log("height ",h," node mumber ",i," total number of trx ",count," trx in this loop ",notrx)        
             }
