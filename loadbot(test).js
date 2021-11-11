0d8f78a4257b78422d92b70679960aa0ba90eb1e
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2021-11-11 12:34:25 +0530

    Update loadbot(test).js

--- loadbot(test).js
+++ loadbot(test).js
@@ -6,6 +6,7 @@ const ContractKit = require('@celo/contractkit')
 const web3 = new Web3('http://localhost:8540')
 const kit = ContractKit.newKitFromWeb3(web3)
 const ethers = require("ethers");
+require('events').EventEmitter.defaultMaxListeners = 0
 
 var count=0;
 
