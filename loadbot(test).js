235e3736416518b7d453fe5e9ca0b4bd3cece63b
Rachit Sonthalia <54906134+rachit77@users.noreply.github.com>
2021-11-16 02:25:36 +0530

    Update loadbot(test).js

--- loadbot(test).js
+++ loadbot(test).js
@@ -16,26 +16,25 @@ async function allTrx()
     
    
     let goldtoken = await kit.contracts.getGoldToken()
-    let notrx=0;
-  
+const mnemonic="owner brisk action valid mammal hold use lake calm huge plug leaf"  
   let Dpath="m/44'/52752'/0'/"+1
-    const wallet = ethers.Wallet.fromMnemonic("alien shell toy depth share work clarify tattoo grass tank master board",Dpath)
+    const wallet = ethers.Wallet.fromMnemonic(mnemonic,Dpath)
     kit.connection.addAccount(wallet.privateKey)
     let senderAddress =wallet.address
     let celoBal = await goldtoken.balanceOf(wallet.address)
     let amount= 10
 
-   for(let i=2; i<=100000000 ;i++)    // parent node loop
+   for(let i=2; i<=100000000000000000 ;i++)    // parent node loop
 
    {
         let DpathChild="m/44'/52752'/0'/"+i
-        const walletChild = ethers.Wallet.fromMnemonic("alien shell toy depth share work clarify tattoo grass tank master board",DpathChild)
+        const walletChild = ethers.Wallet.fromMnemonic(mnemonic,DpathChild)
         let rec=walletChild.address
         
         
            try {
-            let celotrx = goldtoken.transfer(rec, amount).send({from: senderAddress, nonce:i-2 })
-            console.log("transaction number ", count)
+            let celotrx = goldtoken.transfer(rec, amount).send({from: senderAddress })
+            console.log("transaction number ", count, " i is ",i )
             count++;
 
            }
